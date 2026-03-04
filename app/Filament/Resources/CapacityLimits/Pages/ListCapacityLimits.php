66d078256188412c23953ace967208a0d387dc35
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-03-04 23:56:59 +0000

    feat: add Capacity Limits resource under Settings nav group

---
+++ app/Filament/Resources/CapacityLimits/Pages/ListCapacityLimits.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\CapacityLimits\Pages;
+
+use App\Filament\Resources\CapacityLimits\CapacityLimitResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListCapacityLimits extends ListRecords
+{
+    protected static string $resource = CapacityLimitResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make()->slideOver()->modalWidth('2xl'),
+        ];
+    }
+}
