f611708f7f1316720774eec3ac80c6d0c18bd3a9
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-03-04 23:11:47 +0000

    feat: add PayPal invoicing, Instagram caption generator, and customer directory

--- app/Filament/Pages/InstagramCaptionGenerator.php
+++ app/Filament/Pages/InstagramCaptionGenerator.php
@@ -17,7 +17,7 @@ class InstagramCaptionGenerator extends Page implements HasForms
 {
     use InteractsWithForms;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlineCamera;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedCamera;
     protected static string|UnitEnum|null $navigationGroup = 'Marketing';
     protected static ?string $navigationLabel = 'Instagram Captions';
     protected static ?string $title = 'Instagram Caption Generator';
@@ -72,7 +72,7 @@ class InstagramCaptionGenerator extends Page implements HasForms
             Action::make('generate')
                 ->label('Generate Captions')
                 ->action('generateCaptions')
-                ->icon(Heroicon::OutlineSparkles)
+                ->icon(Heroicon::OutlinedSparkles)
                 ->color('primary')
                 ->size('lg'),
         ];
