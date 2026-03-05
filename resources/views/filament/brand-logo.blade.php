47a9922edd6cd88a52d49f1467d0821d4493a882
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-03-05 04:58:08 +0000

    fix: rescue Setting::get in brand-logo blade to prevent crash on central DB

--- resources/views/filament/brand-logo.blade.php
+++ resources/views/filament/brand-logo.blade.php
@@ -1,6 +1,6 @@
 @php
     use App\Models\Setting;
-    $storeName = Setting::get('store_name', 'KneadIt');
+    $storeName = rescue(fn () => Setting::get('store_name', 'KneadIt'), 'KneadIt', false);
 @endphp
 
 <div style="display: flex; align-items: center; height: 100%; font-family: 'Playfair Display', serif; font-size: 1.5rem; font-weight: 700; color: var(--brand-900, #3d2314);">
