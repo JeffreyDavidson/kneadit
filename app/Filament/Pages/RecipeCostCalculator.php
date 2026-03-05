310e2e9507956887568f1b0c52483b979ffc3357
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-03-05 00:20:42 +0000

    fix: syntax error - move property inside class body

--- app/Filament/Pages/RecipeCostCalculator.php
+++ app/Filament/Pages/RecipeCostCalculator.php
@@ -8,8 +8,8 @@ use App\Models\Product;
 use Illuminate\Support\Collection;
 
 class RecipeCostCalculator extends Page
-    protected static bool $shouldRegisterNavigation = false;
 {
+    protected static bool $shouldRegisterNavigation = false;
     protected static string|\BackedEnum|null $navigationIcon = 'heroicon-o-calculator';
     protected static ?string $navigationLabel = 'Recipe Cost Calculator';
     protected static string|\UnitEnum|null $navigationGroup = 'Tools';
