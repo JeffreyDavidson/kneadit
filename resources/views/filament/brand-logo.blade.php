08a01a816b9bd9305398e95cbfe045eff6142bf2
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-03-05 00:47:46 +0000

    style: brand logo bigger (1.5rem) and vertically centered

--- resources/views/filament/brand-logo.blade.php
+++ resources/views/filament/brand-logo.blade.php
@@ -3,6 +3,6 @@
     $storeName = Setting::get('store_name', 'KneadIt');
 @endphp
 
-<div style="font-family: 'Playfair Display', serif; font-size: 1.25rem; font-weight: 700; color: var(--brand-900, #3d2314);">
+<div style="display: flex; align-items: center; height: 100%; font-family: 'Playfair Display', serif; font-size: 1.5rem; font-weight: 700; color: var(--brand-900, #3d2314);">
     {{ $storeName }}
 </div>
