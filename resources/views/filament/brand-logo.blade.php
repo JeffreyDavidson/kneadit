c6e6d423c10696ac46e3a34eeeee10417346ed44
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-03-05 00:46:07 +0000

    fix: brand logo dark brown text on light sidebar header

--- resources/views/filament/brand-logo.blade.php
+++ resources/views/filament/brand-logo.blade.php
@@ -3,6 +3,6 @@
     $storeName = Setting::get('store_name', 'KneadIt');
 @endphp
 
-<div style="font-family: 'Playfair Display', serif; font-size: 1.25rem; font-weight: 700; color: #FFFFFF;">
+<div style="font-family: 'Playfair Display', serif; font-size: 1.25rem; font-weight: 700; color: var(--brand-900, #3d2314);">
     {{ $storeName }}
 </div>
