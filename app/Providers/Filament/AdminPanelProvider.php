631c78889aeead60c950e84a30326e3d5769cec3
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-03-05 01:06:35 +0000

    fix: match brand logo height to bakery (80px)

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -36,7 +36,7 @@ class AdminPanelProvider extends PanelProvider
                 'primary' => Color::hex('#d4920c'),
             ])
             ->brandLogo(view('filament.brand-logo'))
-            ->brandLogoHeight('3.5rem')
+            ->brandLogoHeight('80px')
             ->darkMode(false)
             ->navigationGroups([
                 \Filament\Navigation\NavigationGroup::make('Shop'),
