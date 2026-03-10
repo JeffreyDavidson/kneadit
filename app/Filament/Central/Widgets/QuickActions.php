764b3bc86bc0ee90534f6b7d42ce6d8e05417dbb
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-03-10 14:44:26 +0000

    fix: swap widget views so platform info is left, quick actions right

--- app/Filament/Central/Widgets/QuickActions.php
+++ app/Filament/Central/Widgets/QuickActions.php
@@ -6,9 +6,9 @@ use Filament\Widgets\Widget;
 
 class QuickActions extends Widget
 {
-    protected static ?int $sort = -1;
+    protected static ?int $sort = -2;
 
     protected int|string|array $columnSpan = 1;
 
-    protected string $view = 'filament.central.widgets.quick-actions';
+    protected string $view = 'filament.central.widgets.welcome-banner';
 }
