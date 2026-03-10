18979a2390911fcaf9e48f8db6f289ffb7838080
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-03-10 18:10:16 +0000

    style: make Scheduled Checkins create and edit slide-overs

--- app/Filament/Central/Resources/ScheduledCheckinResource/Pages/ListScheduledCheckins.php
+++ app/Filament/Central/Resources/ScheduledCheckinResource/Pages/ListScheduledCheckins.php
@@ -13,7 +13,8 @@ class ListScheduledCheckins extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()
+                ->slideOver(),
         ];
     }
 }
