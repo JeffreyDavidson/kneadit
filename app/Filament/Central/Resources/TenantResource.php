ea73839a9d261f9233a50994c8e200faf5efa43d
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-03-10 15:28:16 +0000

    fix: DateTimePicker, RichEditor, CheckboxList use Forms namespace not Schemas

--- app/Filament/Central/Resources/TenantResource.php
+++ app/Filament/Central/Resources/TenantResource.php
@@ -6,7 +6,7 @@ use App\Models\Tenant;
 use BackedEnum;
 use Filament\Actions;
 use Filament\Resources\Resource;
-use Filament\Schemas\Components\DateTimePicker;
+use Filament\Forms\Components\DateTimePicker;
 use Filament\Schemas\Components\Grid;
 use Filament\Schemas\Components\Section;
 use Filament\Schemas\Components\Select;
