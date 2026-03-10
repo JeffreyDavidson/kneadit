eaea67a185b080780ad129986fbc9956e8cd1db7
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-03-10 14:40:38 +0000

    fix: platform info left, quick actions right

--- app/Filament/Central/Widgets/QuickActions.php
+++ app/Filament/Central/Widgets/QuickActions.php
@@ -6,7 +6,7 @@ use Filament\Widgets\Widget;
 
 class QuickActions extends Widget
 {
-    protected static ?int $sort = -2;
+    protected static ?int $sort = 0;
 
     protected int|string|array $columnSpan = 1;
 
