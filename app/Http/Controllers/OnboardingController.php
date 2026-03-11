c44813635bae042bdd0ae08ddac201174c7f3ca5
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-03-11 20:06:51 +0000

    fix: pre-fill bakery name on onboarding from registration

--- app/Http/Controllers/OnboardingController.php
+++ app/Http/Controllers/OnboardingController.php
@@ -10,7 +10,9 @@ class OnboardingController extends Controller
 {
     public function show()
     {
-        return view('onboarding');
+        return view('onboarding', [
+            'bakeryName' => session('bakery_name', ''),
+        ]);
     }
 
     public function store(Request $request)
