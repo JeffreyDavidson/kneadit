5d554c19f92d4aa8f88a914f758a4aee4cd74513
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-03-12 04:54:22 +0000

    feat: terms checkbox on registration, 500/503 error pages, .env.example, logo compression + webp

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -22,6 +22,7 @@ class RegisterController extends Controller
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8|confirmed',
             'bakery_name' => 'required|string|max:255',
+            'terms' => 'accepted',
         ]);
 
         $user = User::create([
