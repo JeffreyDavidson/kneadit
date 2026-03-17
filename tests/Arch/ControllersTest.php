781ff531957bab6782eee5aa97e3c229d8e1eec4
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-03-16 23:15:39 -0400

    test: add Pest arch tests for controllers, enums, and debug statements
    - Controllers must be invokable or resourceful (22 currently failing,
      tracked for incremental refactoring)
    - Enums must be string-backed
    - No dd/dump/ray/var_dump left in app code

---
+++ tests/Arch/ControllersTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+$resourceMethods = ['index', 'create', 'store', 'show', 'edit', 'update', 'destroy'];
+
+$controllerFiles = collect(glob(__DIR__.'/../../app/Http/Controllers/**/*.php'))
+    ->merge(glob(__DIR__.'/../../app/Http/Controllers/*.php'))
+    ->map(function ($file) {
+        $relative = str_replace(__DIR__.'/../../app/', '', $file);
+
+        return str_replace(['/', '.php'], ['\\', ''], 'App\\'.$relative);
+    })
+    ->reject(fn ($class) => $class === 'App\\Http\\Controllers\\Controller')
+    ->filter(fn ($class) => class_exists($class))
+    ->filter(fn ($class) => ! (new ReflectionClass($class))->isAbstract())
+    ->values();
+
+foreach ($controllerFiles as $controllerClass) {
+    $shortName = class_basename($controllerClass);
+
+    test("{$shortName} is invokable or resourceful", function () use ($controllerClass, $resourceMethods) {
+        $reflection = new ReflectionClass($controllerClass);
+
+        $publicMethods = collect($reflection->getMethods(ReflectionMethod::IS_PUBLIC))
+            ->reject(fn (ReflectionMethod $method) => $method->class !== $reflection->getName())
+            ->reject(fn (ReflectionMethod $method) => str_starts_with($method->getName(), '__') && $method->getName() !== '__invoke')
+            ->map(fn (ReflectionMethod $method) => $method->getName())
+            ->values()
+            ->all();
+
+        $isInvokable = $publicMethods === ['__invoke'];
+        $isResourceful = empty(array_diff($publicMethods, $resourceMethods));
+
+        expect($isInvokable || $isResourceful)->toBeTrue(
+            'Must be invokable or resourceful. Found: '.implode(', ', $publicMethods)
+        );
+    });
+}
+
+arch('controllers should be classes')
+    ->expect('App\Http\Controllers')
+    ->toBeClasses();
