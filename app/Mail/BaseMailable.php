0dd339f84a7da581ad240e4255314310771f1192
KneadIt Dev <3826104+JeffreyDavidson@users.noreply.github.com>
2026-09-04 18:41:40 -0400

    fix: bound queued mail delivery

--- app/Mail/BaseMailable.php
+++ app/Mail/BaseMailable.php
@@ -9,11 +9,13 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\Attributes\Backoff;
+use Illuminate\Queue\Attributes\Timeout;
 use Illuminate\Queue\Attributes\Tries;
 use Illuminate\Queue\SerializesModels;
 
 #[Tries(3)]
 #[Backoff([10, 60, 300])]
+#[Timeout(60)]
 abstract class BaseMailable extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
