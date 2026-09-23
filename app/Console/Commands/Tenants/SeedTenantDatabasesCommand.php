890bb6994ef8b8b3a2ea958e40c1782316722013
KneadIt Dev <3826104+JeffreyDavidson@users.noreply.github.com>
2026-09-23 10:44:41 -0400

    refactor: use Laravel command signature attribute

--- app/Console/Commands/Tenants/SeedTenantDatabasesCommand.php
+++ app/Console/Commands/Tenants/SeedTenantDatabasesCommand.php
@@ -4,14 +4,13 @@ declare(strict_types=1);
 
 namespace App\Console\Commands\Tenants;
 
+use Illuminate\Console\Attributes\Signature;
 use Stancl\Tenancy\Commands\Seed;
 
-class SeedTenantDatabasesCommand extends Seed
-{
-    protected $signature = 'tenants:seed
-        {class? : The class name of the root seeder}
-        {--class=Database\\Seeders\\DatabaseSeeder : The class name of the root seeder}
-        {--database= : The database connection to seed}
-        {--force : Force the operation to run when in production}
-        {--tenants=* : The tenant(s) to run the command for. Default: all}';
-}
+#[Signature('tenants:seed
+    {class? : The class name of the root seeder}
+    {--class=Database\\Seeders\\DatabaseSeeder : The class name of the root seeder}
+    {--database= : The database connection to seed}
+    {--force : Force the operation to run when in production}
+    {--tenants=* : The tenant(s) to run the command for. Default: all}')]
+class SeedTenantDatabasesCommand extends Seed {}
